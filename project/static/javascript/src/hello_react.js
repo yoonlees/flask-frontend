071261e82a4af138c3c96fdaa44739a4e048c4e5
Yoon Lee <yoonlee@kiwishare.com>
2019-03-04 16:30:07 -0600

    Initial commit

---
+++ project/static/javascript/src/hello_react.js
@@ -0,0 +1,32 @@
+ var RandomMessage = React.createClass({
+  getInitialState: function() {
+    return { message: 'Hello, Universe' };
+  },
+  onClick: function() {
+    var messages = ['Hello, World', 'Hello, Planet', 'Hello, Universe'];
+    var randomMessage = messages[Math.floor((Math.random() * 3))];
+
+    this.setState({ message: randomMessage });
+  },
+  render: function() {
+    return (
+      <div>
+        <MessageView message={ this.state.message }/>
+        <p><input type="button" onClick={ this.onClick } value="Change Message"/></p>
+      </div>
+    );
+  }
+});
+
+var MessageView = React.createClass({
+  render: function() {
+    return (
+      <p>{ this.props.message }</p>
+    );
+  }
+});
+
+ReactDOM.render(
+  <RandomMessage />,
+  document.getElementById('greeting-div')
+);
